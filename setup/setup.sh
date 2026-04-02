6f60b129001ad12efee7874f3a0f90c42827cc7a
KouTashi <tashikou1682@gmail.com>
2026-04-02 12:42:54 +0900

    依存関係を修正

---
+++ setup/setup.sh
@@ -0,0 +1,16 @@
+
+# 依存関係の一括インストール
+# ＊実行方法＊
+# cd ~/ros2_ws/src/setup
+# sudo chmod +x setup.sh
+# ./setup.sh
+
+
+
+# いつもの
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# 以下GUI用
+sudo apt install ros-jazzy-rosbridge-server -y
+
